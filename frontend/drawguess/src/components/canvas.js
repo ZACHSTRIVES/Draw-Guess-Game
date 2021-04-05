a481d8a7bf335f2cbb3f6c25e1f65fb2b9dae092
Thong Teav <ttea943@aucklanduni.ac.nz>
2021-04-05 14:20:49 +1200

    added a drawing canvas

---
+++ frontend/drawguess/src/components/canvas.js
@@ -0,0 +1,9 @@
+// import { ReactSketchCanvas } from 'react-sketch-canvas';
+import CanvasDraw from "react-canvas-draw";
+import './canvas.css';
+
+export default function canvas() {        
+  return (
+    <div></div>
+  );
+}
