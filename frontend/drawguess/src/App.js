42d1825ee378b17797dba24c66fd57e3a7aea96b
Thong Teav <ttea943@aucklanduni.ac.nz>
2021-04-14 17:51:01 +1200

    removed styles

--- frontend/drawguess/src/App.js
+++ frontend/drawguess/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Game from './components/gamepage/gamePage';
+import Game from './components/gameRoom';
 
 function App() {
   return (
---
+++ frontend/drawguess/src/components/gameRoom.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Canvas from './canvas';
+
+export default function GameRoom() {
+  const dense = false;
+  const secondary = false;
+
+  const players = [
+    {
+      'id': 0,
+      'playerName': 'Alan',
+      'score': 10,
+    },
+    {
+      'id': 1,
+      'playerName': 'Eden',
+      'score': 20,
+    },
+    {
+      'id': 2,
+      'playerName': 'Ted',
+      'score': 15,
+    }
+  ];
+
+  return (
+    <div className="root container">
+      <div className="title border">
+        <h5 className="title">TITLE</h5>
+      </div>
+      <div className="canvas border">
+        <Canvas />
+      </div>
+      <div className="round border">
+        <h5 className="title">ROUND</h5>
+        <ul>
+          {players.map((player, index) =>
+            <li key={index} alignItems="flex-start">
+              <div className="player">
+                <List dense={dense}>
+                  <ListItem>
+                    <ListItemText
+                      primary={player.playerName}
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                    <ListItemText
+                      edge="end"
+                      primary={player.score}
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                  </ListItem>
+                </List>
+              </div>
+            </li>)}
+        </ul>
+      </div>
+      <div className="message border">
+        <h5 className="title">MESSAGE</h5>
+      </div>
+    </div>
+  );
+}
