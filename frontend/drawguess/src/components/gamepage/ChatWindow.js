c4390796250087e524d75611224877ca8686a6b8
mhan823 <50857459+mhan823@users.noreply.github.com>
2021-04-14 20:55:57 +1200

    chat function

---
+++ frontend/drawguess/src/components/gamepage/ChatWindow.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+// we are using class component here bcoz functional components cant use react life cycle hooks such as componentDidUpdate
+export default class ChatWindow extends Component {
+  // if this component received new props, move scroll chat window
+  // to view latest message
+  componentDidUpdate = (prevProps, prevState) => {
+    // if component received new props
+    if (this.props.messagesList !== prevProps.messagesList) {
+      // call ref and scroll
+      this.messageListEnd.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  render() {
+    // messagesList the got the messages stored in state
+    // destructuring
+    const { messagesList } = this.props;
+    return (
+      <div className="chat-window">
+        <div className="box">
+          <div className="inner">
+            {Array.isArray(messagesList) &&
+              messagesList.map((message, index) => {
+                if (message.type === "msg") {
+                  return (
+                    <p key={index} className="message">
+                      {message.user}:{message.text}
+                    </p>
+                  )
+                }
+                else if (message.type === "ans") {
+                  return (
+                    <p key={index} className="message">
+                      You got right answer!
+                    </p>
+                  )
+                }
+                else {
+                  return (
+                    <p key={index} className="message">
+                      {message.user} has enter the room
+                    </p>
+                  )
+                }
+              })
+            }
+            {/* define ref and call it if component is updated */}
+            <div
+              className="reference"
+              ref={node => (this.messageListEnd = node)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ frontend/drawguess/src/components/gamepage/chat.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import ChatWindow from "./ChatWindow";
+import ChatComposer from "./ChatComposer";
+import "./styles.css";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        messages: [
+            {   id: 0,
+                user: "john",
+                type: 'msg',
+                text: "First stored message",
+            },
+            {   id: 1,
+                user: "json",
+                type: 'msg',
+                text: "hahaha",
+            },
+            {   id: 2,
+                user: "kido",
+                type: 'ans',
+                text: "vodka",
+            },
+            {   id: 3,
+                user: "batto",
+                type: 'in',
+            },
+          ]
+    };
+  }
+
+  // if new message was submitted from child component // process
+  submitted = getNewMessage => {
+    if (getNewMessage !== "") {
+      // match the state format
+      const newMessage = { text: getNewMessage };
+      // merge new message in copy of state stored messages
+      let updatedMessages = [...this.state.messages, newMessage];
+      // update state
+      this.setState({
+        messages: updatedMessages
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* send stored messages as props to chat window */}
+        <ChatWindow messagesList={this.state.messages} />
+        {/* send submitted props to chat composer */}
+        <ChatComposer submitted={this.submitted} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
