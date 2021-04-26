55750ec9cd3c99c81ec794320972b5a4ce0cde5b
Thong Teav <ttea943@aucklanduni.ac.nz>
2021-04-27 01:07:07 +1200

    added game states for the game room

--- frontend/drawguess/src/components/canvas.js
+++ frontend/drawguess/src/components/canvas.js
@@ -20,7 +20,7 @@ function debounce(fn, ms) {
   };
 }
 
-function Canvas() {
+function Canvas({roomInfo, userName, onChangeGlobalStatus, onChangeGameStatus}) {
   const colors = ['#DB3E00', '#FF6900', '#ffeb3b', '#008B02', '#4caf50', '#03a9f4', '#8ED1FC', '#F78DA7', '#9900EF', '#000000'];
   const words = ['pig', 'rabbit', 'dog', 'starfish', 'bridge', 'library', 'park', 'tower'];
   const DEFAULT_BRUSH_SIZE = 10;
@@ -35,6 +35,7 @@ function Canvas() {
   const [wordChoices, setWordChoices] = useState(null);
   const [startGame, setStartGame] = useState(false);
   const [pause, setPause] = useState(true);
+  const [headerMsg, setHeaderMsg] = useState("");
 
   const deboundCanvasChange = debounce(function handleCanvasChange() {
     const canvas = canvasRef.current;
@@ -107,6 +108,7 @@ function Canvas() {
     console.log("handle select word for word: ", word);
     setWord(word);
     setPause(false);
+    onChangeGameStatus("drawing");
   }
 
   const handleGamePause = () => {
@@ -116,11 +118,17 @@ function Canvas() {
 
     var results = words.slice(0, 3);
     setWordChoices(results);
+    onChangeGameStatus("ChoosingWord");
   }
 
   const handleStartGame = () => {
     console.log("starting game btn pressed");
     setStartGame(true);
+    onChangeGlobalStatus("playing");
+  }
+
+  const maskWord = (word) => {
+    return word.replace(/\S/gi, '_ ');
   }
 
   const debouncedHandleResize = debounce(function handleResize() {
@@ -160,12 +168,31 @@ function Canvas() {
 
   return (
     <div className="canvas-container flex-center-all" ref={containerRef} >
-      { (!startGame) ? <StartGameMask isHost={true} onStartGame={handleStartGame}></StartGameMask> :
-       (pause) && <WordSelectionMask words={words.slice(0, 3)} onSelectWord={handleSelectWord}></WordSelectionMask>}
+      {console.log("room info in canvas: ", roomInfo)}
+      {(() => {
+        console.log("Global status: ", roomInfo.globalStatus);
+        // const isDrawer = roomInfo.game.drawer === userName;
+        const isDrawer = true;
+        if (roomInfo.globalStatus === "waiting") {
+          return (<StartGameMask isHost={isDrawer} onStartGame={handleStartGame}></StartGameMask>);
+        }
+        else if (roomInfo.globalStatus === "playing") {
+          if (roomInfo.game.status === "ChoosingWord") {
+            return (<WordSelectionMask isDrawer={isDrawer} words={words.slice(0, 3)} onSelectWord={handleSelectWord}></WordSelectionMask>);
+          }
+          else if (roomInfo.game.status === "drawing") {
+            // isDrawer ? setHeaderMsg(`You're drawing: ${word}`) : setHeaderMsg(`The drawing is: ${maskWord(word)}`);
+          }
+          else {
+            //finish round return scoreboard
+          }
+        }
+        else {
+          //finish game
+        }
+      })()}
       <div className="canvas-header glass-rect">
-        {console.log("Pause: ", pause)}
-        {console.log("start game: ", startGame)}
-        <span>You're drawing: {word}</span>
+        <span>{false ? `You're drawing: ${word}` : `The drawing is: ${maskWord(word)}`}</span>
         <Timer gameOn={!pause} onPause={handleGamePause} />
         {/* <div className="canvas-timer flex-center-all"></div> */}
       </div>
@@ -181,7 +208,7 @@ function Canvas() {
       <div className="tools-overlay flex-center-all">
         {
           (showOption !== "none") &&
-          <div className="tools-container flex bottom-offset">
+          <div className="tools-container flex bottom-offset glass-rect-bright rounded-rect">
             {
               (showOption === "size") &&
               <div className="size-options">
@@ -207,7 +234,7 @@ function Canvas() {
           </div>
         }
 
-        <div className="tools-container flex bottom">
+        <div className="tools-container flex bottom glass-rect-bright rounded-rect">
           <div className="tool flex-center-all" onClick={handleStrokeColorToggle}>
             <div className="tool-btn flex-center-all color-btn">
               <Palette style={{ color: brushColor }} />
--- frontend/drawguess/src/components/wordSelectionMask.js
+++ frontend/drawguess/src/components/wordSelectionMask.js
@@ -1,16 +1,21 @@
 import Button from '@material-ui/core/Button';
 
-export default function WordSelectionMask({words, onSelectWord}) {
+export default function WordSelectionMask({ isDrawer, words, onSelectWord }) {
   return (
     <div className="mask-container flex-center-all mask rounded-rect">
-      <div className="pop-up-container flex-center-all flex-column rounded-rect glass-rect">
-        <p>Choose a word:</p>
-        <div className="options-btn-group">
-          <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[0])}>{words[0]}</Button>
-          <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[1])}>{words[1]}</Button>
-          <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[2])}>{words[2]}</Button>
+      {isDrawer ?
+        <div className="pop-up-container flex-center-all flex-column rounded-rect glass-rect">
+          <p>Choose a word:</p>
+          <div className="options-btn-group">
+            <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[0])}>{words[0]}</Button>
+            <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[1])}>{words[1]}</Button>
+            <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[2])}>{words[2]}</Button>
+          </div>
+        </div> :
+        <div className="pop-up-container flex-center-all flex-column rounded-rect glass-rect">
+          <p>Waiting for player to choose a word...</p>
         </div>
-      </div>      
+      }
     </div>
   );
 }
