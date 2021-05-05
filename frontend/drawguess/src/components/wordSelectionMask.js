881d848bce4bcb7c29723e44a4afb785d318e8a7
qcao819 <qcao819@aucklanduni.ac.nz>
2021-05-05 21:00:10 +1200

    Update wordSelectionMask.js

--- frontend/drawguess/src/components/wordSelectionMask.js
+++ frontend/drawguess/src/components/wordSelectionMask.js
@@ -15,7 +15,7 @@ export default function WordSelectionMask({ isDrawer, words, onSelectWord, socke
     <div className="mask-container flex-center-all mask rounded-rect">
       {isDrawer ?
         <div className="pop-up-container flex-center-all flex-column rounded-rect glass-rect">
-          <p>Timer: {timer}</p>
+          <div className="mask-timer canvas-timer flex-center-all">{timer}</div>
           <p>Choose a word:</p>
           <div className="options-btn-group">
             <Button variant="outlined" className="option-btn" onClick={() => onSelectWord(words[0])}>{words[0]}</Button>
