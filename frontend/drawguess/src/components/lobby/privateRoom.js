9d09d0149cccbeba0a1cc1b9a210c34fa1a80a79
Zach <wangziqi0325@gmail.com>
2021-05-06 14:30:57 +1200

    Private room password verification

--- frontend/drawguess/src/components/lobby/privateRoom.js
+++ frontend/drawguess/src/components/lobby/privateRoom.js
@@ -11,78 +11,96 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Alert from '@material-ui/lab/Alert';
 
 
 const useStyles = makeStyles({
-    root: {
-      Width: 400,
-      height: 100,
-    },
-    title: {
-      fontSize: 14,
-      textAlign: 'left',
-    },
-    pos: {
+  root: {
+    Width: 400,
+    height: 100,
+  },
+  title: {
+    fontSize: 14,
+    textAlign: 'left',
+  },
+  pos: {
     //   marginBottom: 12,
-      textAlign: 'right',
-    },
-    name: {
-        textAlign: 'left',
-    },
-    icon:{
-      display: 'inline-flex',
-    }
-  });
+    textAlign: 'right',
+  },
+  name: {
+    textAlign: 'left',
+  },
+  icon: {
+    display: 'inline-flex',
+  }
+});
 
-export default function PublicRoomCard({room}) {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-    const handleOpen=()=>{
-        setOpen(true);
-    }
+export default function PublicRoomCard({ room,joinRoom}) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [password, setPassword] = React.useState("");
+  const [passwordAlert,setPasswordAlert] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  }
 
-    const handleClose = () => {
+  const handleClose = () => {
+    setOpen(false);
+    setPasswordAlert(false);
+  };
+
+  const handleJoinPrivateRoom = () => {
+    if(password===room.password){
       setOpen(false);
-    };
+      setPasswordAlert(false);
+      joinRoom(room.roomID);
+
+      
+    }else{
+      
+      setPasswordAlert(true);
+    }
+
+  };
 
 
-    return  (
+  return (
     <li>
-    <Card className="{classes.root} margin_top_card" onClick={handleOpen}>
+      <Card className="{classes.root} margin_top_card" onClick={handleOpen}>
         <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
             ID {room.roomID}
-        </Typography>
-        <Typography className={classes.name} variant="h5" component="h2">
+          </Typography>
+          <Typography className={classes.name} variant="h5" component="h2">
             {room.roomName}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
             {/* <GroupIcon className={classes.icon}/> */}
-            <GroupIcon style={{verticalAlign: 'middle'}}/>
+            <GroupIcon style={{ verticalAlign: 'middle' }} />
             {room.currentPlayers}/{room.maxPlayers}
-        </Typography>
+          </Typography>
         </CardContent>
-    </Card>
-    <Dialog
+      </Card>
+      <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{room.roomName}</DialogTitle>
+        <DialogTitle id="alert-dialog-title"> Private Room</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {room.roomName}
-          </DialogContentText>
+          <input type="password" onChange={e=>setPassword(e.target.value)} placeholder="Room Password"></input>
+          {passwordAlert===false?<a></a>:<Alert severity="error">Incorrect Password!</Alert>}
+        
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
-            Disagree
+            cancel
           </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
+          <Button onClick={handleJoinPrivateRoom} color="primary" autoFocus>
+            enter
           </Button>
         </DialogActions>
       </Dialog>
-   </li>)
+    </li>)
 }
