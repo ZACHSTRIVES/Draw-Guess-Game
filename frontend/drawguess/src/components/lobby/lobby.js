5e9ea0e31e3c15228e57f1cada2b0e0a4382a30d
Thong Teav <ttteav@gmail.com>
2021-05-08 22:57:45 +1200

    added highest score chart

--- frontend/drawguess/src/components/lobby/lobby.js
+++ frontend/drawguess/src/components/lobby/lobby.js
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles, styled } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
+import React, { useState } from 'react';
 import RoomList from './roomList';
-import axios from 'axios'
 import CreateRoom from '../CreateRoom/CreateRoomModal/CreateRoomModal';
+import Statistics from './statistics';
+import {
+  useHistory
+} from "react-router-dom";
+
 import logo from '../../static/drawguesslogo.png';
 import publicRoom from '../../static/publicRoom.png';
 import privateRoom from '../../static/privateRoom.png';
 import allRoom from '../../static/allRoom.png';
-import {
-  Redirect, useHistory
-} from "react-router-dom";
-import './lobby.css';
-import { ContactSupportOutlined, SportsRugbySharp } from '@material-ui/icons';
-import Statistics from './statistics';
 
+import './lobby.css';
 
 const config = {
   headers: {
@@ -27,28 +20,9 @@ const config = {
   }
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    background: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: '24px',
-  },
-  iconButton: {
-    padding: 10,
-  },
-
-
-}));
-
-
 export default function Lobby({ socket, userName, rooms, isLogin }) {
   const [roomType, setRoomType] = useState("All");
   const history = useHistory();
-
-  const classes = useStyles();
-
   const [stats, setStats] = React.useState({
     rounds: 0,
     firstRanks: 0,
@@ -130,8 +104,6 @@ export default function Lobby({ socket, userName, rooms, isLogin }) {
       <div className="left flex">
         <div className="left-top glass-blur flex flex-column">
           <div className="logo-bg lobby-title">
-            {/* Draw and Guess */}
-            {/* <img src={logo} alt="logo"></img> */}
             <img src={logo} alt="logo"></img>
           </div>
           <div className="account-bg">            
@@ -156,14 +128,6 @@ export default function Lobby({ socket, userName, rooms, isLogin }) {
         <div className="left-btm glass-blur flex flex-column">
           <div className="room-banner flex">
             <div className="room-type lobby-title text-title">{roomType} Rooms</div>
-            <div className="search-room">
-              <Paper component="form" className={classes.root}>
-                <input className="search-input" placeholder="Room number"></input>
-                <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                  <SearchIcon />
-                </IconButton>
-              </Paper>
-            </div>
           </div>
           <CreateRoom socket={socket} handleCreateRoom={handleCreateRoom}></CreateRoom>
           <RoomList rooms={rooms} joinRoom={handleJoinRoom} show={roomType}></RoomList>
@@ -173,16 +137,6 @@ export default function Lobby({ socket, userName, rooms, isLogin }) {
         <div className="stats-banner lobby-title text-title">Statistics</div>
         <Statistics data={stats} />
       </div>
-      {/* <img src={logo} className="logo"></img>
-      <div className="online_info">Hello, {userName}</div>
-
-      
-      <br />
-      <CreateRoom socket={socket} handleCreateRoom={handleCreateRoom}></CreateRoom>
-      <br />
-      <div>
-        <RoomList rooms={rooms} joinRoom={handleJoinRoom} show={"public"}></RoomList>
-      </div> */}
     </div>
   );
 }
--- frontend/drawguess/src/components/lobby/statistics.js
+++ frontend/drawguess/src/components/lobby/statistics.js
@@ -29,63 +29,27 @@ export default function Statistics({ data }) {
           <div className="stats-first-desc flex-center-all">
             <div className="stats-rank-time text-subtitle"><span className="text-bold">{data.firstRanks}</span> times</div>
           </div>
+          <div className="stats-highest-score flex-column flex-center-all">            
+            <div className="chart-title">Highest Score</div>
+            <div><span className="text-bold">//highest score</span></div>            
+            <div className="chart-title">Time</div>
+            <div><span className="record-col-value">//time</span></div>
+          </div>
         </div>
         <div className="stats-right flex flex-column">
           <div className="stats-rounds flex-column flex-center-all">
             <div><span className="text-bold">{data.rounds}</span></div>
             <div className="chart-title">Rounds Played</div>
           </div>
-          <ChartCard title={"2nd Rank"} count={data.secondRanks} percentage={100} />
+          <ChartCard title={"2nd Rank"} count={data.secondRanks} percentage={data.secondRate} />
           <ChartCard title={"3rd Rank"} count={data.thirdRanks} percentage={data.thirdRate} />
-          {/* <div className="stats-second stats-rank flex">
-            <div className="stats-rank-title text-subtitle">
-              <div className="stats-rank-title-desc flex">
-                <ProgressBar
-                  completed={60}
-                  className="stats-rank-bar"
-                  baseBgColor={"rgba(0, 0, 0, 0.3)"}
-                  transitionDuration="1s"
-                  bgColor={"rgba(255, 255, 255, 1)"}
-                  labelColor="black"
-                  height="12px"
-                />
-              </div>
-            </div>
-            <div className="stats-rank-desc text-subtitle">2nd Rank</div>
-          </div> */}
-          {/* <div className="stats-third stats-rank flex">
-            <div className="stats-rank-title text-subtitle">
-              <div className="stats-rank-title-desc flex">
-                <ProgressBar
-                  completed={30}
-                  className="stats-rank-bar"
-                  baseBgColor={"rgba(0, 0, 0, 0.3)"}
-                  transitionDuration="1s"
-                  bgColor={"rgba(255, 255, 255, 1)"}
-                  labelColor="black"
-                  height="12px"
-                />
-              </div>
-            </div>
-            <div className="stats-rank-desc text-subtitle">3rd Rank</div>
-          </div> */}
         </div>
-
-        {/* <div className="stats-rounds flex-column stats-bubble stats-ball">
-          <div className="stats-bubble-title">Rounds</div>
-          <div className="stats-bubble-value">3</div>
-        </div>
-        <div className="stats-first flex-column stats-bubble stats-ball">
-          <div className="stats-bubble-title">1st</div>
-          <div className="stats-bubble-value">2</div>
-        </div> */}
       </div>
       <div className="records">
         <div className="record-title lobby-title text-title">
           Records
         </div>
         <div className="record-list">
-          {/* map */}
           {data.history.map((history) =>
             <div className="record-card flex">
               <div className="record-rank flex flex-column record-col">
@@ -101,7 +65,6 @@ export default function Statistics({ data }) {
                 <div className="record-col-value">{history.score}</div>
               </div>
             </div>
-
           )}
         </div>
       </div>
