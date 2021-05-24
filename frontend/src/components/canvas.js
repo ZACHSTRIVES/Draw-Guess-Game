bbe86de3625b6551bfb3fc29bec96209e496eb68
Thong Teav <ttteav@gmail.com>
2021-05-25 01:11:51 +1200

    show which player is choosing the word

--- frontend/src/components/canvas.js
+++ frontend/src/components/canvas.js
@@ -236,7 +236,7 @@ function Canvas({ roomInfo, userName, socket }) {
         }
         else if (roomInfo.globalStatus === "playing") {
           if (roomInfo.game.status === "ChoosingWord") {
-            return (<WordSelectionMask isDrawer={isDrawer} words={randomWords} onSelectWord={handleSelectWord} socket={socket}>  </WordSelectionMask>);
+            return (<WordSelectionMask drawer={roomInfo.game.drawer} isDrawer={isDrawer} words={randomWords} onSelectWord={handleSelectWord} socket={socket}>  </WordSelectionMask>);
           }
           else if (roomInfo.game.status === "drawing") {
             return (<div className="canvas-header glass-rect">
